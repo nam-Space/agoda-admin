ba8f165d8ae9011ea70445b9419d7266b22d856e
PTITer2003 <namyahoo2003@gmail.com>
2025-12-12 09:27:35 +0700

    fix collapse description in hotel and room module

--- src/components/dashboard/hotel/ModalHotelDetail.tsx
+++ src/components/dashboard/hotel/ModalHotelDetail.tsx
@@ -6,6 +6,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
 import RoomTable from "../room/RoomTable";
 import { FaHotel } from "react-icons/fa";
+import { useEffect, useState } from "react";
 
 interface IProps {
     hotel?: any | null;
@@ -15,6 +16,33 @@ interface IProps {
 
 const ModalHotelDetail = (props: IProps) => {
     const { hotel, isModalOpen, setIsModalOpen } = props;
+    const [isExpanded, setIsExpanded] = useState({
+        mostFeature: false,
+        facilities: false,
+        withUs: false,
+        usefulInformation: false,
+        description: false,
+        amenitiesAndFacilities: false,
+        locationInfo: false,
+        nearbyLocation: false,
+        regulation: false
+    });
+
+    useEffect(() => {
+        if (!isModalOpen) {
+            setIsExpanded({
+                mostFeature: false,
+                facilities: false,
+                withUs: false,
+                usefulInformation: false,
+                description: false,
+                amenitiesAndFacilities: false,
+                locationInfo: false,
+                nearbyLocation: false,
+                regulation: false
+            })
+        }
+    }, [isModalOpen])
 
     return (
         <ConfigProvider locale={vi_VN}>
@@ -91,39 +119,158 @@ const ModalHotelDetail = (props: IProps) => {
                                 <p><strong>Địa điểm:</strong> {hotel?.location} m²</p>
                                 <div className="mt-[10px]">
                                     <p><strong>Điểm nổi bật nhất:</strong></p>
-                                    <div dangerouslySetInnerHTML={{ __html: hotel?.mostFeature || "" }}></div>
+                                    {hotel?.mostFeature && <div dangerouslySetInnerHTML={{
+                                        __html: hotel.mostFeature.length > 150 ? (isExpanded.mostFeature
+                                            ? hotel.mostFeature
+                                            : `${hotel.mostFeature.substring(0, 150)}...`) : hotel.mostFeature
+                                    }}></div>}
+                                    {hotel?.mostFeature?.length > 150 && <button
+                                        onClick={() => setIsExpanded({
+                                            ...isExpanded,
+                                            mostFeature: !isExpanded.mostFeature
+                                        })}
+                                        className="text-blue-600 hover:underline text-sm inline-block"
+                                    >
+                                        {isExpanded.mostFeature ? "Ẩn" : "Xem tiếp"}
+                                    </button>}
                                 </div>
                                 <div className="mt-[10px]">
                                     <p><strong>Cơ sở vật chất:</strong></p>
-                                    <div dangerouslySetInnerHTML={{ __html: hotel?.facilities || "" }}></div>
+                                    {hotel?.facilities && <div dangerouslySetInnerHTML={{
+                                        __html: hotel.facilities.length > 150 ? (isExpanded.facilities
+                                            ? hotel.facilities
+                                            : `${hotel.facilities.substring(0, 150)}...`) : hotel.facilities
+                                    }}></div>}
+                                    {hotel?.facilities?.length > 150 && <button
+                                        onClick={() => setIsExpanded({
+                                            ...isExpanded,
+                                            facilities: !isExpanded.facilities
+                                        })}
+                                        className="text-blue-600 hover:underline text-sm inline-block"
+                                    >
+                                        {isExpanded.facilities ? "Ẩn" : "Xem tiếp"}
+                                    </button>}
                                 </div>
                                 <div className="mt-[10px]">
                                     <p><strong>Về chúng tôi:</strong></p>
-                                    <div dangerouslySetInnerHTML={{ __html: hotel?.withUs || "" }}></div>
+                                    {hotel?.withUs && <div dangerouslySetInnerHTML={{
+                                        __html: hotel.withUs.length > 150 ? (isExpanded.withUs
+                                            ? hotel.withUs
+                                            : `${hotel.withUs.substring(0, 150)}...`) : hotel.withUs
+                                    }}></div>}
+                                    {hotel?.withUs?.length > 150 && <button
+                                        onClick={() => setIsExpanded({
+                                            ...isExpanded,
+                                            withUs: !isExpanded.withUs
+                                        })}
+                                        className="text-blue-600 hover:underline text-sm inline-block"
+                                    >
+                                        {isExpanded.withUs ? "Ẩn" : "Xem tiếp"}
+                                    </button>}
                                 </div>
                                 <div className="mt-[10px]">
                                     <p><strong>Thông tin hữu ích:</strong></p>
-                                    <div dangerouslySetInnerHTML={{ __html: hotel?.usefulInformation || "" }}></div>
+                                    {hotel?.usefulInformation && <div dangerouslySetInnerHTML={{
+                                        __html: hotel.usefulInformation.length > 150 ? (isExpanded.usefulInformation
+                                            ? hotel.usefulInformation
+                                            : `${hotel.usefulInformation.substring(0, 150)}...`) : hotel.usefulInformation
+                                    }}></div>}
+                                    {hotel?.usefulInformation?.length > 150 && <button
+                                        onClick={() => setIsExpanded({
+                                            ...isExpanded,
+                                            usefulInformation: !isExpanded.usefulInformation
+                                        })}
+                                        className="text-blue-600 hover:underline text-sm inline-block"
+                                    >
+                                        {isExpanded.usefulInformation ? "Ẩn" : "Xem tiếp"}
+                                    </button>}
                                 </div>
                                 <div className="mt-[10px]">
                                     <p><strong>Mô tả:</strong></p>
-                                    <div className="markdown-container" dangerouslySetInnerHTML={{ __html: hotel?.description || "" }}></div>
+                                    {hotel?.description && <div className="markdown-container" dangerouslySetInnerHTML={{
+                                        __html: hotel.description.length > 150 ? (isExpanded.description
+                                            ? hotel.description
+                                            : `${hotel.description.substring(0, 150)}...`) : hotel.description
+                                    }}></div>}
+
+                                    {hotel?.description?.length > 150 && <button
+                                        onClick={() => setIsExpanded({
+                                            ...isExpanded,
+                                            description: !isExpanded.description
+                                        })}
+                                        className="text-blue-600 hover:underline text-sm inline-block"
+                                    >
+                                        {isExpanded.description ? "Ẩn" : "Xem tiếp"}
+                                    </button>}
+
                                 </div>
                                 <div className="mt-[10px]">
                                     <p><strong>Tiện nghi và cơ sở vật chất:</strong></p>
-                                    <div dangerouslySetInnerHTML={{ __html: hotel?.amenitiesAndFacilities || "" }}></div>
+                                    {hotel?.amenitiesAndFacilities && <div dangerouslySetInnerHTML={{
+                                        __html: hotel.amenitiesAndFacilities.length > 150 ? (isExpanded.amenitiesAndFacilities
+                                            ? hotel.amenitiesAndFacilities
+                                            : `${hotel.amenitiesAndFacilities.substring(0, 150)}...`) : hotel.amenitiesAndFacilities
+                                    }}></div>}
+                                    {hotel?.amenitiesAndFacilities?.length > 150 && <button
+                                        onClick={() => setIsExpanded({
+                                            ...isExpanded,
+                                            amenitiesAndFacilities: !isExpanded.amenitiesAndFacilities
+                                        })}
+                                        className="text-blue-600 hover:underline text-sm inline-block"
+                                    >
+                                        {isExpanded.amenitiesAndFacilities ? "Ẩn" : "Xem tiếp"}
+                                    </button>}
                                 </div>
                                 <div className="mt-[10px]">
                                     <p><strong>Vị trí:</strong></p>
-                                    <div dangerouslySetInnerHTML={{ __html: hotel?.locationInfo || "" }}></div>
+                                    {hotel?.locationInfo && <div dangerouslySetInnerHTML={{
+                                        __html: hotel.locationInfo.length > 150 ? (isExpanded.locationInfo
+                                            ? hotel.locationInfo
+                                            : `${hotel.locationInfo.substring(0, 150)}...`) : hotel.locationInfo
+                                    }}></div>}
+                                    {hotel?.locationInfo?.length > 150 && <button
+                                        onClick={() => setIsExpanded({
+                                            ...isExpanded,
+                                            locationInfo: !isExpanded.locationInfo
+                                        })}
+                                        className="text-blue-600 hover:underline text-sm inline-block"
+                                    >
+                                        {isExpanded.locationInfo ? "Ẩn" : "Xem tiếp"}
+                                    </button>}
                                 </div>
                                 <div className="mt-[10px]">
                                     <p><strong>Đi đâu gần đây:</strong></p>
-                                    <div dangerouslySetInnerHTML={{ __html: hotel?.nearbyLocation || "" }}></div>
+                                    {hotel?.nearbyLocation && <div dangerouslySetInnerHTML={{
+                                        __html: hotel.nearbyLocation.length > 150 ? (isExpanded.nearbyLocation
+                                            ? hotel.nearbyLocation
+                                            : `${hotel.nearbyLocation.substring(0, 150)}...`) : hotel.nearbyLocation
+                                    }}></div>}
+                                    {hotel?.nearbyLocation?.length > 150 && <button
+                                        onClick={() => setIsExpanded({
+                                            ...isExpanded,
+                                            nearbyLocation: !isExpanded.nearbyLocation
+                                        })}
+                                        className="text-blue-600 hover:underline text-sm inline-block"
+                                    >
+                                        {isExpanded.nearbyLocation ? "Ẩn" : "Xem tiếp"}
+                                    </button>}
                                 </div>
                                 <div className="mt-[10px]">
                                     <p><strong>Quy định của chỗ nghỉ:</strong></p>
-                                    <div dangerouslySetInnerHTML={{ __html: hotel?.regulation || "" }}></div>
+                                    {hotel?.regulation && <div className="markdown-container" dangerouslySetInnerHTML={{
+                                        __html: hotel.regulation.length > 150 ? (isExpanded.regulation
+                                            ? hotel.regulation
+                                            : `${hotel.regulation.substring(0, 150)}...`) : hotel.regulation
+                                    }}></div>}
+                                    {hotel?.regulation?.length > 150 && <button
+                                        onClick={() => setIsExpanded({
+                                            ...isExpanded,
+                                            regulation: !isExpanded.regulation
+                                        })}
+                                        className="text-blue-600 hover:underline text-sm inline-block"
+                                    >
+                                        {isExpanded.regulation ? "Ẩn" : "Xem tiếp"}
+                                    </button>}
                                 </div>
                             </div>
                         </div>
